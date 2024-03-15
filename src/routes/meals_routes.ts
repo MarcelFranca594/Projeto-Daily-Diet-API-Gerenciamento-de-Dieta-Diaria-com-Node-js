311e99ef76aac924739db924f4d15c3bf326d46b
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-03-15 10:54:19 -0400

    Rota para atualizar os detalhes de uma refeição com base no ID da refeição

--- src/routes/meals_routes.ts
+++ src/routes/meals_routes.ts
@@ -47,4 +47,72 @@ export async function mealsRoutes(app: FastifyInstance) {
       return reply.send({ meals })
     },
   )
+
+  // Rota para visualizar os detalhes de uma única refeição com base no ID da refeição
+  app.get(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] },
+    async (request, reply) => {
+      // Define um esquema de validação para os parâmetros da URL
+      const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+      const { mealId } = paramsSchema.parse(request.params)
+
+      // Busca no banco de dados a refeição com o ID fornecido
+      const meal = await knex('meals').where({ id: mealId }).first()
+
+      // Se não encontrar a refeição, retorna uma resposta com status 404 (Not Found)
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      // Retorna uma resposta com os dados da refeição encontrada
+      return reply.send({ meal })
+    },
+  )
+
+  // Rota para atualizar os detalhes de uma refeição com base no ID da refeição
+  app.put(
+    '/:mealId',
+    { preHandler: [checkSessionIdExists] }, // Middleware para verificar se o ID da sessão existe
+    async (request, reply) => {
+      // Define um esquema de validação para os parâmetros da URL
+      const paramsSchema = z.object({ mealId: z.string().uuid() })
+
+      // Extrai o ID da refeição dos parâmetros da URL e valida-o
+      const { mealId } = paramsSchema.parse(request.params)
+
+      // Define um esquema de validação para os dados da refeição a serem atualizados
+      const updateMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        isOnDiet: z.boolean(),
+        date: z.coerce.date(), // Converte o valor para o tipo 'Date'
+      })
+
+      // Extrai os dados da refeição do corpo da requisição e valida-os
+      const { name, description, isOnDiet, date } = updateMealBodySchema.parse(
+        request.body,
+      )
+
+      // Busca no banco de dados a refeição com o ID fornecido
+      const meal = await knex('meals').where({ id: mealId }).first()
+
+      // Se não encontrar a refeição, retorna uma resposta com status 404 (Not Found)
+      if (!meal) {
+        return reply.status(404).send({ error: 'Meal not found' })
+      }
+
+      // Atualiza os detalhes da refeição no banco de dados
+      await knex('meals').where({ id: mealId }).update({
+        name,
+        description,
+        is_on_diet: isOnDiet,
+        date: date.getTime(), // Converte o valor da data para milissegundos
+      })
+
+      // Retorna uma resposta com status 204 (No Content) indicando que a atualização foi bem-sucedida
+      return reply.status(204).send()
+    },
+  )
 }
